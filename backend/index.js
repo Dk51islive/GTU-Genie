01840a32e02e6eb674ce0cf1a0ef45327782e552
Dk <128335792+Dk51islive@users.noreply.github.com>
2025-04-20 03:35:10 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import authRoutes from './routes/authRoutes.js'
+import subscriptionRoutes from './routes/subscriptionRoutes.js'
+import earningsRoutes from './routes/earningsRoutes.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://gtu-genie', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.use('/api/auth', authRoutes)
+app.use('/api/subscription', subscriptionRoutes)
+app.use('/api/earnings', earningsRoutes)
+
+app.get('/', (req, res) => {
+  res.send('GTU Genie Backend is Live!')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
